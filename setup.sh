6cae7017e98c536ccee97a5b6d5617a7fcaf1fc7
Grzegorz Płaczek <g01.placzek@gmail.com>
2022-08-12 21:32:19 +0200

    Update preseed

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-DISK_IMAGE=$IMAGES_DIR/storage.img
 IMAGES_DIR=images
-DEBIAN_ISO=debian-11.4.0-amd64.iso
+DISK_IMAGE=$IMAGES_DIR/storage.img
+DEBIAN_ISO=$IMAGES_DIR/debian-11.4.0-amd64.iso
 DEBIAN_URL=https://cdimage.debian.org/debian-cd/current/amd64/iso-dvd/debian-11.4.0-amd64-DVD-1.iso
 DEBIAN_DIR=$IMAGES_DIR/debian
 LAG_ISO=rocket-lag-debian.iso
@@ -14,9 +14,9 @@ setup_debian() {
         # Download debian image if not present
         if [ ! -f $IMAGES_DIR/$DEBIAN_ISO ]; then
             echo "No debian iso, downloading..."
-            wget -q -O $IMAGES_DIR/$DEBIAN_ISO $DEBIAN_URL
+            wget -q -O $DEBIAN_ISO $DEBIAN_URL
         fi
-        bsdtar -C $DEBIAN_DIR -xf $IMAGES_DIR/$DEBIAN_ISO
+        bsdtar -C $DEBIAN_DIR -xf $DEBIAN_ISO
     fi
 }
 
@@ -40,9 +40,9 @@ build_image() {
 }
 
 run_iso() {
-    qemu-system-x86_64 -m 4G -smp 2 \
-    -hda $IMAGES_DIR/$DISK_IMAGE \
-    -cdrom $IMAGES_DIR/$DEBIAN_ISO  \
+    qemu-system-x86_64 -m 4G -smp 4 \
+    -hda $DISK_IMAGE \
+    -cdrom $IMAGES_DIR/$LAG_ISO \
     -boot d -enable-kvm
 }
 
